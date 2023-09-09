f7447c80f4bce4bb0bbfed594ef6f9b499af355a
murilLost <63795081+murilLost@users.noreply.github.com>
2023-09-09 16:48:13 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,6 +50,7 @@ resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
+  key_name               = "Devops-ON"
 
   user_data = <<-EOF
               #!/bin/bash
